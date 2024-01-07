1365c8160383a0e476b8f1ca498574e44a32adff
mayurrohokale <mayurrohokale12345@gmail.com>
2024-01-07 20:34:30 +0530

    parent to child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { AfterContentChecked, AfterContentInit, Component, OnInit } from '@angular/core';
 import { Directive } from '@angular/core';
 @Component({
-  selector: 'app-demo',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })

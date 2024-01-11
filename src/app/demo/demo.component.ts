c8a919adaf7b1a27ac792631459614521b527d60
mayurrohokale <mayurrohokale12345@gmail.com>
2024-01-11 23:46:16 +0530

    propert decorator

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -13,5 +13,6 @@ export class DemoComponent implements AfterContentInit {
   ngAfterContentInit(): void {
     const content = this.paraRef?.nativeElement;
     content.style.fontStyle = 'Italic';
+    content.style.fontColor = 'red';
   }
 }

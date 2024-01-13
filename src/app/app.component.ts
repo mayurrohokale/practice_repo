bb738299ca58677150bd5ee846d342f82ced9e28
mayurrohokale <mayurrohokale12345@gmail.com>
2024-01-13 15:25:20 +0530

    pipes in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { AfterContentChecked, AfterContentInit, AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Directive } from '@angular/core';
 import { LoginComponent } from './login/login.component';
+import { DatePipe } from '@angular/common';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,19 +12,9 @@ import { LoginComponent } from './login/login.component';
 
 
 
-export class AppComponent implements AfterViewInit {
-  ngAfterViewInit(): void {
-    if(this.btnRef?.nativeElement){
-      this.btnRef.nativeElement.innerHTML = "Counter ++"; // hide the button
-    }
-  }
-  @ViewChild(LoginComponent) logincomponent?: LoginComponent;
-  @ViewChild('btnctr') btnRef?:ElementRef<HTMLButtonElement>;
+export class AppComponent {
 
-incrementCnt(){
-  console.log(this.logincomponent);
-  this.logincomponent?.incrementCnt();
-}
+  birthday = new Date(1988, 3, 15);
 
 
 

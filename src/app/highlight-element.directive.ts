015156d75edc4ae3676f78e6974439919a340b07
mayurrohokale <mayurrohokale12345@gmail.com>
2024-01-13 16:49:09 +0530

    HostBinding and HostListner

--- src/app/highlight-element.directive.ts
+++ src/app/highlight-element.directive.ts
@@ -8,6 +8,7 @@ export class HighlightElementDirective implements OnInit {
   @HostBinding('style.backgroundColor') bgcolor?:string;
   @HostBinding('style.color') textcolor?:string;
 
+  
   ngOnInit(): void {
     this.bgcolor = 'aqua';
     this.textcolor = 'red';

1bb2833adc33d1f6cf00161f0cb5477c5c55d6aa
mayurrohokale <mayurrohokale12345@gmail.com>
2024-01-14 23:17:52 +0530

    Number Pipe in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,58 +14,7 @@ import { DatePipe } from '@angular/common';
 
 export class AppComponent {
 
-  products:{name:string; imageUrl:string}[]=[
-    {
-      name:'Pre placement Activity',
-      imageUrl:'./assests/1.jpg',
-    },
-    {
-        name:"Post Placement Activity",
-        imageUrl:'./assests/2.png'
-    },
-    {
-      name:"Placement Drive",
-      imageUrl:'./assests/3.jpg'
-    },
-    {
-      name:"Placement Drive",
-      imageUrl:'./assests/3.jpg'
-    },
-    {
-      name:"Placement Drive",
-      imageUrl:'./assests/3.jpg'
-    },
-    {
-      name:"Placement Drive",
-      imageUrl:'./assests/3.jpg'
-    },
-    {
-      name:"Placement Drive",
-      imageUrl:'./assests/3.jpg'
-    },
-    {
-      name:"Placement Drive",
-      imageUrl:'./assests/3.jpg'
-    },
-    {
-      name:"Placement Drive",
-      imageUrl:'./assests/3.jpg'
-    }
-  ];
-
-  pgSize: number =4;
-  startIndex: number = 0;
-  endIndex: number = this.pgSize;
-
-  priviousPage(){
-    this.startIndex -= this.pgSize;
-    this.endIndex -= this.pgSize;
-  }
-
-  nextPage(){
-    this.startIndex += this.pgSize;
-    this.endIndex += this.pgSize;
-  }
+  pi = 3.1459;
 }
 
 

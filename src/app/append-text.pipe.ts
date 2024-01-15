0832c0ffa7a16ef4a1a8ebae8a4113d7c9305343
mayurrohokale <mayurrohokale12345@gmail.com>
2024-01-15 12:18:51 +0530

    impure pipe

--- src/app/append-text.pipe.ts
+++ src/app/append-text.pipe.ts
@@ -1,7 +1,8 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'appendText'
+  name: 'appendText',
+  pure: false
 })
 export class AppendTextPipe implements PipeTransform {
 

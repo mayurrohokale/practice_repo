8d8dd7e1d7d85bfed9628daab42f887cc84a5b4d
mayurrohokale <mayurrohokale12345@gmail.com>
2024-01-16 16:04:36 +0530

    redirecting routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,9 @@ const routes: Routes = [
   {
     path: 'login', component: LoginComponent, children:[
       {
+        path: '', redirectTo: 'signup' ,  pathMatch: 'full'
+      },
+      {
       
         path: 'signup', component: SignupComponent
       },
